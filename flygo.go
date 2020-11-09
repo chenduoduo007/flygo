e4f750bf0739bda4e1aeace48a98136fbd05c793
chenchen <809930354@qq.com>
2020-11-09 10:50:48 +0800

    删除Static Handler

--- flygo.go
+++ flygo.go
@@ -2,7 +2,6 @@ package flygo
 
 import (
 	"net/http"
-	"path"
 	"strings"
 )
 
@@ -102,30 +101,6 @@ func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
 // POST请求
 func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
 	group.addRoute("POST", pattern, handler)
-}
-
-// create static handler
-func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
-	absolutePath := path.Join(group.prefix, relativePath)
-	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
-	return func(c *Context) {
-		file := c.Param("filepath")
-		// Check if file exists and/or if we have permission to access it
-		if _, err := fs.Open(file); err != nil {
-			c.Status(http.StatusNotFound)
-			return
-		}
-
-		fileServer.ServeHTTP(c.Writer, c.Req)
-	}
-}
-
-// serve static files
-func (group *RouterGroup) Static(relativePath string, root string) {
-	handler := group.createStaticHandler(relativePath, http.Dir(root))
-	urlPattern := path.Join(relativePath, "/*filepath")
-	// Register GET handlers
-	group.GET(urlPattern, handler)
 }
 
 
